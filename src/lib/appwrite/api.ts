8581878c20c8dd1b810a0d489aba939619522cba
Fei <wengangfei@gmail.com>
2024-04-17 00:07:23 -0400

    added

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { INewUser } from "@/types";
 import { ID } from 'appwrite';
-import { account } from "./config";
+import { account, appwriteConfig, avatars, database } from "./config";
 
 //define the user type in typescript
 export async function createNewUser(user: INewUser){
@@ -13,9 +13,42 @@ export async function createNewUser(user: INewUser){
             user.password,
             user.username,
         )
+
+        if(!newUserAccount) throw Error;
+        const avatarURL = avatars.getInitials(user.username);
+
+        const newUser = await saveUserToDB({
+            accountID: newUserAccount.$id,
+            name: newUserAccount.name,
+            email: newUserAccount.email,
+            username: user.username,
+            imageURL: avatarURL,
+        })
+
         return newUserAccount;
     }catch(error){
         console.log(error);
         return error
+    }
+}
+
+
+export async function saveUserToDB(user: {
+    accountID: string;
+    name: string;
+    email: string;
+    username: string;
+    imageURL: URL;
+}){
+    try{
+        const newUser = await database.createDocument(
+            appwriteConfig.databaseID,
+            appwriteConfig.userCollectionID,
+            ID.unique(),
+            user,
+        )
+        return newUser;
+    }catch(error){
+        console.log(error)
     }
 }
